1601c725dc972c48993b41f6c91e58b9e7ba927a
harrymich <66319179+harrymich@users.noreply.github.com>
2023-07-07 20:50:24 +0100

    7th Jule PM

--- src/pages/To_Do_List.py
+++ src/pages/To_Do_List.py
@@ -2,7 +2,7 @@ import dash
 from dash import dcc, html
 
 dash.register_page(__name__, name='To-do List')
-git ad
+
 todo = ['Compare change in rate and split after stride. Think about presenting in a table or in a graph instead. Maybe '
         'highlight 5 strokes after stride on graph and then have text output below.',
         'Find a way to plot piece comparison against distance and time and not just stroke rate',
